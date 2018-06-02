92c42bfe0cc6a808ef4c38b315bb493d9c1c0044
LABICSC02-65961 <erwinsalas42@gmail.com>
2018-06-01 22:01:29 -0600

    Cambios

--- server.py
+++ server.py
@@ -1,18 +1,48 @@
+from flask import Flask, request, render_template,redirect,url_for
 
-__author__ = 'Erwin'
-#!flask/bin/python
-from flask import Flask,request,render_template
+
+import mysql.connector
+
+
+
+
+
+from werkzeug.utils import secure_filename
+import os
+from flask import send_from_directory
+
+
+
+
+
+cnx = mysql.connector.connect(user='root', password='123',
+                              host='127.0.0.1',
+                              database='ansibleBase')
 
 app = Flask(__name__)
 
-@app.route('/login',method=['POST'])
+@app.route('/')
+def index():
+    return "working!"
+
+
+@app.route('/login', methods=['GET','POST'])
 def login():
-    req_data = request.get_json()
-
-    user = req_data['username']
-    password = req_data['password']
-    python_version = req_data['access']
 
 
+    if request.method == 'GET':
+        return render_template('loginform.html')
 
-#  keygen
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(port=8090, debug=True)
+
+
+cnx.close()
